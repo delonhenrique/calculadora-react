6ceae686012fd7274c79268d0f7d7d3fdc84f56d
Delon Henrique <delon.henrique@gmail.com>
2022-05-31 02:10:26 -0300

    Calculator - React

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,16 @@ import React from "react";
 import "./Button.css"
 
 const Button = (props) => {
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
     return (
-        <button className="button">{props.label}</button>
+        <button
+            onClick={e => props.click && props.click(e.target.innerHTML)}
+            className={classes}>
+            {props.label}
+        </button>
     )
 }
 
